f89d9514d873dcdb919eec30eb8115ff5e8f099d
Nathan Baliteau <nathanbaliteau@gmail.com>
2020-02-19 15:55:25 +0100

    index events welcome page added

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all
+Event.destroy_all
 
-10.times do
-  user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Quote.yoda, email: Faker::Name.first_name.downcase + "@yopmail.com")
+20.times do
+  user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Quote.yoda, email: Faker::Name.first_name.downcase + "@yopmail.com", password: Faker::Alphanumeric.alpha(number: 8))
+end
+
+6.times do
+  event = Event.create!(start_date: Faker::Date.forward(days: 60), duration: Faker::Number.within(range: 12..100) * 5, title: Faker::Coffee.blend_name, description: Faker::GreekPhilosophers.quote, price: Faker::Number.between(from: 1, to: 1000), location: Faker::Restaurant.name, admin: User.all.sample)
 end
